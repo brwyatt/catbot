fd2e919119d4178577db927f5fd1b97042f72643
Bryan Wyatt <brwyatt@gmail.com>
2018-06-07 20:18:34 -0700

    Switch over to irc3 framework

--- run.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python3
-
-from catbot import CatBot
-
-catbot = CatBot()
-
-catbot.start()
---
+++ setup.py
@@ -0,0 +1,25 @@
+#!/user/bin/env python
+
+from setuptools import setup
+
+setup(
+    name='catbot',
+    version='0.1.0',
+    author='Bryan Wyatt',
+    author_email='brwyatt@gmail.com',
+    description=('IRC bot'),
+    license='GPLv3',
+    keywords='irc bot',
+    url='https://github.com/brwyatt/catbot',
+    packages=['catbot'],
+    package_dir={'': 'src'},
+    include_package_data=False,
+    entry_points={
+        'console_scripts': [
+            'catbot = irc3:run'
+        ],
+    },
+    install_requires=[
+        'irc3==1.0.3'
+    ]
+)
