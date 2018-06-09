724a6d97cf8a21a8beeebf7d24bacd7a5ce06869
Bryan Wyatt <brwyatt@gmail.com>
2018-06-08 17:24:31 -0700

    Annotate bot plugin imports

--- src/catbot/plugins/core.py
+++ src/catbot/plugins/core.py
@@ -12,9 +12,9 @@ class Core:
         self.log = logging.getLogger('irc3.{0}'.format(module))
         self.log.debug('Loading Core plugins')
 
-        bot.include('irc3.plugins.command')
-        bot.include('irc3.plugins.ctcp')
-        bot.include('irc3.plugins.uptime')
-        bot.include('catbot.plugins.nickserv')
+        bot.include('irc3.plugins.command')  # Configuration for !commands
+        bot.include('irc3.plugins.ctcp')  # Various handling for CTCP
+        bot.include('irc3.plugins.uptime')  # !uptime
+        bot.include('catbot.plugins.nickserv')  # NickServ identify
 
         self.log.debug('Core plugins loaded!')
