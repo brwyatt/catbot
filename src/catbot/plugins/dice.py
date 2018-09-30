b9e656d6fda048451a482c72d6bd69f44dbfc446
Bryan Wyatt <brwyatt@gmail.com>
2018-09-30 16:04:59 -0700

    Move dice plugin strings to DynamoDB
    This lets us obey language preferences, leveraging the previous work
    done for the greetings.

--- src/catbot/plugins/dice.py
+++ src/catbot/plugins/dice.py
@@ -4,10 +4,36 @@ import irc3
 from irc3.plugins.command import command
 
 from catbot.plugin import Plugin
+from catbot.plugins.user_prefs import UserPrefs
 
 
 @irc3.plugin
 class Dice(Plugin):
+    userprefs = None
+
+    def __init__(self, bot):
+        super().__init__(bot)
+
+        if Dice.userprefs is None:
+            Dice.userprefs = UserPrefs(bot)
+
+    def get_string(self, string_id, lang, default):
+        string_id = string_id.lower()
+        lang = lang.lower()
+        default_lang = self.userprefs.defaults.get('lang', 'eng').lower()
+
+        string = self.data.get_string(string_id, lang=lang,
+                                      namespace=self.module)
+
+        if string is None and lang != default_lang:
+            string = self.data.get_string(string_id, lang=default_lang,
+                                          namespace=self.module)
+
+        if string is None:
+            string = default
+
+        return string
+
     @command()
     def roll(self, mask, target, args):
         """Roll dice
@@ -20,30 +46,42 @@ class Dice(Plugin):
         count = 1
         sides = 6
 
+        lang = self.userprefs.get_pref(mask.nick, 'lang')
+
         if '<count>' in args and args['<count>'] is not None:
             try:
                 count = int(args['<count>'])
             except:
-                return 'ERROR: I can\'t roll a non-numeric amount of things!'
+                return self.get_string(
+                    'error_non-numeric_count', lang,
+                    'ERROR: non-numeric count').format(count=args['<count>'])
 
         if '<sides>' in args and args['<sides>'] is not None:
             try:
                 sides = int(args['<sides>'])
             except:
-                return 'ERROR: I can\'t roll something with non-numeric sides!'
+                return self.get_string(
+                    'error_non-numeric_sides', lang,
+                    'ERROR: non-numeric sides').format(sides=args['<sides>'])
 
         if count < 1:
-            return 'ERROR: I have to roll SOMETHING!'
+            return self.get_string(
+                'error_too_few_dice', lang,
+                'ERROR: too few dice').format(count=count)
         elif count > 1000000:
-            return 'I don\'t have that many dice to roll!'
+            return self.get_string(
+                'error_too_many_dice', lang,
+                'ERROR: too many dice').format(count=count)
 
         if sides < 2:
-            return 'Error: Cannot roll something with less than 2 sides!'
+            return self.get_string(
+                'error_too_few_sides', lang,
+                'ERROR: too few sides').format(sides=sides)
 
         total = 0
         for i in range(count):
             total += random.randint(1, sides)
 
-        return 'Rolled {count} D{sides} and got {total}!'.format(count=count,
-                                                                 sides=sides,
-                                                                 total=total)
+        return self.get_string(
+            'success', lang, '{count}xD{sides} = {total}!'
+        ).format(count=count, sides=sides, total=total)
