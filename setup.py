6bd9bec5f3b7d40d4debe5f780a36fd559956e6d
Bryan Wyatt <brwyatt@gmail.com>
2018-10-03 18:27:05 -0700

    Update version number

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='catbot',
-    version='0.1.0',
+    version='0.2.0',
     author='Bryan Wyatt',
     author_email='brwyatt@gmail.com',
     description=('IRC bot'),
